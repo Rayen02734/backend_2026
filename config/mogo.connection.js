6a25b67fb7c05e7fc888761ef26461627962bb9b
Rayen02734 <sebairayen2006@gmail.com>
2026-08-26 11:19:58 +0100

    modification en cnx de bd

--- config/mogo.connection.js
+++ config/mogo.connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports.connectToMongoDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://aziz:Azerty.123@cluster0.bldeeml.mongodb.net/").then(() => {
+    await mongoose.connect(process.env.mongo_url).then(() => {
       console.log('MongoDB is connected');
     }).catch((error) => {
       console.error(`Erreur de connexion à MongoDB: ${error.message}`);
